f9e41cbb92ee89456e6f15362dd916fe5ed48bc2
Ricardo Valero de la Rosa <55701657+RicardoValero95@users.noreply.github.com>
2023-07-28 17:46:14 -0600

    bitset cleanup

--- src/internal_effect_untraced/bitset.ts
+++ src/internal_effect_untraced/bitset.ts
@@ -1,14 +1,33 @@
-import * as Chunk from "@effect/data/Chunk"
+import { pipe } from "@effect/data/Function"
 import * as Number from "@effect/data/Number"
 import * as ReadonlyArray from "@effect/data/ReadonlyArray"
 import type * as BitSet from "@effect/parser/BitSet"
 
 /** @internal */
 export const fromIterable = (bits: Iterable<number>): BitSet.BitSet =>
-  ReadonlyArray.sort(ReadonlyArray.fromIterable(new Set(bits)), Number.Order) as BitSet.BitSet
+  pipe(
+    new Set(bits),
+    ReadonlyArray.sort(Number.Order)
+  ) as BitSet.BitSet
 
 /** @internal */
-export const make = (...bits: ReadonlyArray<number>): BitSet.BitSet => ReadonlyArray.sort(new Set(bits), Number.Order)
+export const fromChars = (chars: Iterable<string>): BitSet.BitSet =>
+  pipe(
+    ReadonlyArray.fromIterable(chars),
+    ReadonlyArray.map((char) => char.charCodeAt(0)),
+    fromIterable
+  )
 
 /** @internal */
-export const allChars: BitSet.BitSet = fromIterable(Chunk.range("\u0000".charCodeAt(0), "￿".charCodeAt(0)))
+export const toChars = (codes: BitSet.BitSet): Iterable<string> =>
+  pipe(
+    codes,
+    ReadonlyArray.map((code) => String.fromCharCode(code))
+  )
+
+/** @internal */
+export const all: BitSet.BitSet = pipe(
+  ["\u0000", "\uFFFD"] as const,
+  fromChars,
+  ([start, end]) => ReadonlyArray.range(start, end)
+) as BitSet.BitSet
